bb8aaef56db815907c79412d1e2c9e7c5d0acf91
Treparme <62477124+Treparme@users.noreply.github.com>
2020-04-10 12:11:01 +0200

    Update audio_processing.py

--- audio_processing.py
+++ audio_processing.py
@@ -187,7 +187,7 @@ try:
                 window = buffered_window[delay+index: delay+windowsize + index]
                 
                 output[index] = -np.dot(window, filter_)
-                if counter%8 == 0:
+                if counter%4 == 0:
                     window_delay = buffered_window[index:windowsize + index]
 
                     error = error_input[index]
