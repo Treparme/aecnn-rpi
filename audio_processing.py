f9b6562e66f95748e9ed668102faed0f8a8c20f1
Treparme <62477124+Treparme@users.noreply.github.com>
2020-05-03 18:14:11 +0200

    Update audio_processing.py

--- audio_processing.py
+++ audio_processing.py
@@ -118,7 +118,7 @@ noisy=np.zeros((1,model_blocksize,1),dtype=precision)
 data=np.zeros((blocksize,),dtype=precision)
 
 
-mu = 4/delay #learning rate
+mu = 2/delay #learning rate
 epsilon = 10**(-3)
 
 try:
